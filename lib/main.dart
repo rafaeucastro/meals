761103b2d5b627ce29f3b772a7ff8c6a62e6c3fa
Rafael Castro <castrorafael456@gmail.com>
2022-06-24 10:55:26 -0300

    Initial state

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'screens/categories_screen.dart';
+
+void main() => runApp(const Meals());
+
+class Meals extends StatelessWidget {
+  const Meals({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Vamos cozinhar?",
+      home: const CategoriesScreen(),
+      theme: ThemeData(
+        fontFamily: 'Raleway',
+        canvasColor: const Color.fromARGB(255, 254, 229, 225),
+        textTheme: ThemeData.light().textTheme.copyWith(
+                titleMedium: const TextStyle(
+              fontSize: 20,
+              fontFamily: 'RobotoCondensed',
+            )),
+        colorScheme: ColorScheme.fromSwatch(
+          accentColor: Colors.amber,
+          primarySwatch: Colors.pink,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:meals/data/dummy_data.dart';
+import '../components/category_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Vamos Cozinhar?"),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(20),
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: DUMMY_CATEGORIES.map((cat) {
+          return CategoryItem(cat);
+        }).toList(),
+      ),
+    );
+  }
+}
